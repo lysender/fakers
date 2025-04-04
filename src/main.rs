fca6e1571d7ca1e57fba3e665695acbed1081baf
Lysender <lysender@darkstar-win>
2025-04-04 09:52:28 +0800

    fix: initial import

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use fake::{Fake, faker::company::en::CatchPhrase};
+
+fn main() {
+    let title: String = CatchPhrase().fake();
+    let description: String = format!(
+        "{}, {}, {}, {}",
+        CatchPhrase().fake::<String>(),
+        CatchPhrase().fake::<String>(),
+        CatchPhrase().fake::<String>(),
+        CatchPhrase().fake::<String>()
+    );
+
+    println!("{title}\n{description}\n");
+}
